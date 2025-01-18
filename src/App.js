65947ed34c3a8539362fe8c5c975d0ad459a84e4
Julio Lopez <58756482+donlopez@users.noreply.github.com>
2025-01-17 22:32:30 -0500

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,44 @@
+// App.js
+
 import { useAuth } from "react-oidc-context";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 function App() {
   const auth = useAuth();
-  const navigate = useNavigate(); // Initialize navigate
 
-  // Sign-out redirection
   const signOutRedirect = () => {
-    const clientId = "55b82psg55qubr8q6dmbrliq2u"; // Your Client ID from Cognito
-    const logoutUri = "https://main.d27hasdhmsk331.amplifyapp.com/"; // The URI to redirect to after logout
-    const cognitoDomain = "https://us-east-1m61qxqqmo.auth.us-east-1.amazoncognito.com"; // Your Cognito User Pool domain
+    const clientId = "55b82psg55qubr8q6dmbrliq2u";
+    const logoutUri = "https://https://main.d27hasdhmsk331.amplifyapp.com/";
+    const cognitoDomain = "https://us-east-1m61qxqqmo.auth.us-east-1.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
-  // Loading state
   if (auth.isLoading) {
     return <div>Loading...</div>;
   }
 
-  // Error handling
   if (auth.error) {
     return <div>Encountering error... {auth.error.message}</div>;
   }
 
-  // When the user is authenticated
   if (auth.isAuthenticated) {
-    // Navigate to home page after authentication
-    navigate("/home");
-
     return (
       <div>
-        <h2>Welcome, {auth.user?.profile?.email}!</h2>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
         <button onClick={() => auth.removeUser()}>Sign out</button>
       </div>
     );
   }
 
-  // When the user is not authenticated
   return (
     <div>
       <button onClick={() => auth.signinRedirect()}>Sign in</button>
-      <button onClick={() => signOutRedirect()}>Sign out (Redirect)</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
     </div>
   );
 }
-
+  
 export default App;
