a4e02b12e3300e9daf58803047f2179cb9f5f807
Julio Lopez <58756482+donlopez@users.noreply.github.com>
2025-01-29 14:03:09 -0500

    modified:   src/sidebar.js 	modified:   src/styles.css

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for routing
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
       <button className="close-btn" onClick={toggleSidebar}>×</button>
       <ul>
-        <li><a href="/profile">Profile</a></li>
-        {/* Add more tabs as needed */}
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/planit">Planit</Link></li> {/* Added Planit tab */}
+        <li><Link to="/dashboard">Dashboard</Link></li> {/* Added Dashboard tab */}
       </ul>
     </div>
   );
 };
 
 export default Sidebar;
-
